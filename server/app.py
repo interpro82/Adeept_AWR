0689146d496e356a497bc2c7d53694d0e1cb0f14
iliya <iliya.nikolov@sirma.bg>
2022-10-25 21:54:36 +0300

    fixed ws

--- server/app.py
+++ server/app.py
@@ -58,10 +58,6 @@ def sendgen(filename):
 def index():
     return send_from_directory(dir_path+'/dist', 'index.html')
 
-@app.route('/api/location')
-def get_location():
-    print("getting location")
-
 class webapp:
     def __init__(self):
         self.camera = camera
