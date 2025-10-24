e726856abbcc0c538c35dec7c4b2d48f485d39d4
ilia <iliyanikolov82@gmail.com>
2025-10-24 23:20:37 +0300

    handle command

--- server/webServer.py
+++ server/webServer.py
@@ -524,7 +524,22 @@ def get_location(app):
 
 def handle_command(cmd):
     print(f"Command received: {cmd}")
+    global direction_command, turn_command
+    if 'MOVE_UP' == cmd:
+        direction_command = 'forward'
+        move.move(speed_set, 'forward', 'no', rad)
 
+    elif 'MOVE_DOWN' == cmd:
+        direction_command = 'backward'
+        move.move(speed_set, 'backward', 'no', rad)
+
+    elif 'MOVE_LEFT' == cmd:
+        turn_command = 'left'
+        move.move(speed_set, 'no', 'left', rad)
+
+    elif 'MOVE_RIGHT' == cmd:
+        turn_command = 'right'
+        move.move(speed_set, 'no', 'right', rad)
 
 if __name__ == '__main__':
     switch.switchSetup()
